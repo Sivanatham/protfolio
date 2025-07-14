5aef5e94567da9ccf5c0f07dfcc16e843780bdb6
Siva Nathan <siva3736nathan@gmail.com>
2025-07-14 16:40:55 +0530

    Initial commit with GSAP animated portfolio

--- pro.js
+++ pro.js
@@ -1,13 +1,98 @@
-var p1 = document.querySelector("#p1")
-var p2 = document.querySelector("#p2")
+ gsap.to(".d2",{
+        x:100,
+        duration:2,
+        ease:"power2.out"
+    }); 
+gsap.from('#sp',{
+    y:200,
+    duration:1.7,
+    ease:"power1.in"
+});    
+// gsap.to(".d3",{
+//         x:100,
+//         duration:2,
+//         ease:"power2.out"
+
+        
+//     }); 
+// gsap.from('.d4',{
+//     z:10,
+//     duration:1.7,
+//     ease:"power1.in"
+// });    
 
 
-var b1 =document.querySelector(".b1")
-b1.addEventListener("click",()=>{
-    p1.classList.toggle("hide")
-    p2.classList.toggle("hide")// if class have it remove the class and it no class add class 
+gsap.fromTo("#d5",
+  { opacity: 0, y: 100 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.9,
+    scrollTrigger: {
+      trigger: "#d5",
+      start: "top 90%",
+      toggleActions: "play reverse play reverse",
+    }
+  }
+);
+gsap.to('.d4',{
+    x:100,
+    duration:3,
+    ease:"power1.in"
+}); 
+gsap.from('.right',{
+    x:100,
+    duration:3,
+    ease:"power1.in"
+}); 
+gsap.to('.left',{
+    x:100,
+    duration:3,
+    ease:"power1.in"
+}); 
+gsap.fromTo("#ph",
+  { opacity: 0, y: 100 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.9,
+    scrollTrigger: {
+      trigger: "#ph",
+      start: "top 90%",
+      toggleActions: "play reverse play reverse",
+    }
+  }
+);
+gsap.to('#project1',{
+    x:100,
+    duration:3,
+    ease:"power1.in",
+    scrollTrigger: {
+      trigger: "#project1",
+      start: "top 90%",
+      toggleActions: "play reverse play reverse",
+    }
+}); 
+gsap.to('#contact',{
+    x:100,
+    duration:3,
+    ease:"power.in",
+    scrollTrigger: {
+      trigger: "#contact",
+      start: "top 90%",
+      toggleActions: "play reverse play reverse",
+    }
+}); 
+gsap.from('#infom',{
+    y:100,
+    duration:3,
+    ease:"power1.in",
+    scrollTrigger: {
+      trigger: "#infom",
+      start: "top 90%",
+      toggleActions: "play reverse play reverse",
+    }
+}); 
 
-    
-    b1.textContent = btn.textContent==="show more"?"show less":"show more"
+ 
 
-    })
